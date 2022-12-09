4a1383bae20b5239d797a4a73615ac992dcc73ce
Felipe Serna <felipe.serna@jala-foundation.org>
2022-12-08 21:43:34 -0500

    First commit

---
+++ WebApplication1_learning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1_learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
